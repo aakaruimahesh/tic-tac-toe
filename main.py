c1253113ba5c99f9205a34ffb2aa9f2f0d0ab8f1
Aakarui <maheshmaharjanc1@gmail.com>
2020-11-19 22:43:12 +0545

    assert exception for game

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ from start_game import StartGame
 
 
 if __name__ == "__main__":
-    keep_playing = True
-    while keep_playing:
-        start_game = StartGame()
-        start_game.run()
+    try:
+        keep_playing = True
+        while keep_playing:
+            start_game = StartGame()
+            start_game.run()
 
-        play_again = input('Do you want to play new game again? (Y / N or any key to exit): ')
-        if play_again.lower() != 'y':
-            keep_playing = False
+            play_again = input('Do you want to play new game again? (Y / N or any key to exit): ')
+            if play_again.lower() != 'y':
+                keep_playing = False
+    except AssertionError:
+        print('GameError. Input the valid ROW_SIZE')
+    except Exception as e:
+        print('Error: ' + str(e))
